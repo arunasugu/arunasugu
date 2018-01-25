40d6d82d2b907027a3486b42701a145ac94e59f9
arunasugu <35652990+arunasugu@users.noreply.github.com>
2018-01-25 16:43:20 +0530

    Update frnds.c

--- frnds.c
+++ frnds.c
@@ -2,10 +2,14 @@
 #include<conio.h>
 void main()
 {
-int a;
-for(a=1;a<=3;a++)
-{
-printf("hello\n");
-}
-getch{};
-}
+int n,i,sum=0;
+  printf("enter a value");
+  scanf("%d",&n);
+        for(i=1;i<=n;++i)
+        {
+          sum+=i;
+        }
+        printf("sum %d",sum);
+        return 0
+        getch();
+        }
